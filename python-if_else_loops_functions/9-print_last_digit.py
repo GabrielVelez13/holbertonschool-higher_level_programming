fee7f00ba5dbf0facb08eb78533ee312c183eef8
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2024-01-24 11:30:09 -0400

    fixing checkers 2

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     newNumber = int(str(number)[-1:])
-    print(newNumber, end="")
+    print("{}".format(newNumber), end="")
     return newNumber
