31e4a7842e8312b1e4e4c3226ab7020f4b388cfd
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2024-01-26 19:51:55 -0400

    testing files

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,3 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         print(" ".join(map(str, line)))
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    lastNum = number % -10
+    ln = number % -10
 else:
-    lastNum = number % 10
-if lastNum > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, lastNum))
-elif lastNum == 0:
-    print("Last digit of {} is {} and is 0".format(number, lastNum))
+    ln = number % 10
+firstString = "Last digit of {} is {} and".format(number, ln)
+if ln > 5:
+    print("{} is greater than 5".format(firstString))
+elif ln == 0:
+    print("{} is 0".format(firstString))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastNum))
+    print("{} is less than 6 and not 0".format(firstString))
