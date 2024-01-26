afec75336396f4b225c3985130233557f4580fc9
Gabriel Velez <gabrielvelez@Gabriels-MBP.localdomain>
2024-01-26 09:55:31 -0400

    fixing bugs and adding code

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     total = 0
     numArgs = len(argv) - 1
     for i in range(numArgs):
-        total =+ int(argv[i + 1])
+        total += int(argv[i + 1])
     print(total)
