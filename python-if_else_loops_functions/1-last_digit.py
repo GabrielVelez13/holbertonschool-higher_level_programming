112b2d1ab3232d10727cb24202e10847a8f119ea
Gabriel Velez <gabrielvelez@Gabriels-MBP.localdomain>
2024-01-26 09:24:56 -0400

    adding python imports

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
-num = random.randint(-10000, 10000)
-lastNum = int(str(num)[-1:])
-if num < 0:
-    lastNum *= -1
-if lastNum > 5:
-    print("Last digit of {} is {} and is greater than 5".format(num, lastNum))
-elif lastNum == 0:
-    print("Last digit of {} is {} and is 0".format(num, lastNum))
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastNum = number % -10
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(num, lastNum))
+    lastNum = number % 10
+if lastNum > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, lastNum))
+elif lastNum == 0:
+    print("Last digit of {} is {} and is 0".format(number, lastNum))
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastNum))
---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    total = 0
+    numArgs = len(argv) - 1
+    for i in range(numArgs):
+        total =+ int(argv[i + 1])
+    print(total)
