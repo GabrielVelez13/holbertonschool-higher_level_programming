53cfa39f6b51ebd83259b0c1e0c09583774feabf
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2024-01-31 20:37:06 -0400

    fixing name error in tasl 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -14,5 +14,18 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
             print("out of range")
         finally:
-            new_list.append(div)
+            newList.append(div)
     return newList
+
+
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
