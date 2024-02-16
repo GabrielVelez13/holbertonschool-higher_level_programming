04a1fd8d4ebc2e6feb05d29d4a9c0e6c070d2bd1
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-02-16 15:02:34 -0400

    adding task 17.2, fixed testing

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -57,6 +57,6 @@ class Base:
     def from_json_string(json_string):
         """ Changing json to dictionary. """
         if not json_string:
-            return "[]"
+            return []
         else:
             return json.loads(json_string)
