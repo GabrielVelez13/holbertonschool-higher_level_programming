81c56081a766bcde09c496967cebc5b54ad1a056
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 18:15:54 -0400

    adding task 16

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- Lists all the scores and names from the second_table table, ordered by score in descending order, but only for those records where the name is not null.
+SELECT score, name
+FROM hbtn_0c_0.second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
