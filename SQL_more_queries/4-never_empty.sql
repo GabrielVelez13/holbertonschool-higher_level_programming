6a4e34f257e6d82a5e7e406537a09b6c48b4acf9
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 19:20:11 -0400

    adding task 5

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
---
+-- create a table where id defaults to 1.
 CREATE table if NOT EXISTS id_not_null(
     id INT default 1,
     name VARCHAR(256)
---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- 
+CREATE table if NOT EXISTS unique_id(
+    id INT UNIQUE default 1,
+    name VARCHAR(256)
+);
