a263bbfcfc5ca4096ebd6f07705fa625894d993a
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 19:16:14 -0400

    adding task 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Create table where the name is forced to be not null
-CREATE table if NOT EXISTS force_name{
-    id INT;
-    name VARCHAR(256) NOT NULL;
-}
+CREATE TABLE IF NOT EXISTS force_name(
+     id INT,
+     name VARCHAR(256) NOT NULL
+);
