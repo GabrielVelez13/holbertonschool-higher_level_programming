8f3f38387b876a5695cafb0b3b3a40e1340f3898
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 17:46:10 -0400

    adding task 6

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Writes script that prints the content of a table.
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
