9e249c571a8080881c9061363f55c0bbfe08cb07
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 22:45:32 -0400

    adding task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,8 @@
 -- Count shows by genre
-SELECT genre, COUNT(*) AS number_of_shows
-FROM tv_genres
-GROUP BY genre
-HAVING COUNT(*) > 0
+SELECT g. name AS genre,
+COUNT(*) AS number_of_shows
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS t
+ON g. id = t. genre_id
+GROUP BY g. name
 ORDER BY number_of_shows DESC;
