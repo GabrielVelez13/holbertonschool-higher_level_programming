378acb8d886d5cb81395825d8e4fcf818945b3e3
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 22:32:27 -0400

    adding task 9

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-INNER JOIN tv_show_genres ON hbtn_0d_tvshows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT s. title, g. genre_id
+FROM tv_shows AS s
+INNER JOIN tv_show_genres AS g
+ON s. id = g. show_id
+ORDER BY s. title, g. genre_id;
