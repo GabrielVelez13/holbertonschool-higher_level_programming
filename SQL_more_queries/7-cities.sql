2a7f14f95adfd88af67c8f0053e02bafdf645958
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 22:10:34 -0400

    adding task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 \c hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    state_id INT FOREIGN KEY REFERENCES states(id) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    state_id INT REFERENCES states(id) NOT NULL,
     name VARCHAR(256) NOT NULL
 );
