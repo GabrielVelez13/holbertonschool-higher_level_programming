d9226b7af18ef1b6bc9b3723801ef2a70487eaf1
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 22:03:02 -0400

    adding task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,7 @@
 -- Create a database and a table with the following requirements:
-CREATE database if NOT EXISTS hbtn_0c_usa;
-USE hbtn_0c_usa;
-CREATE table if NOT EXISTS states(
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
-
-
