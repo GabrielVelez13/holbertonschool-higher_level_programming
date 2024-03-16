92cc6554f64cc00a13a36e307552bb4722feffb7
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 22:52:26 -0400

    adding task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,9 @@
 -- 
-SELECT tv_shows.title
-FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE title = 'Dexter'
-ORDER BY tv_shows.title;
+SELECT g. name
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS s
+ON g. id = s. genre_id
+INNER JOIN tv_shows AS t
+ON t. id = s. show_id
+WHERE t. title = "Dexter"
+ORDER BY g. name;
