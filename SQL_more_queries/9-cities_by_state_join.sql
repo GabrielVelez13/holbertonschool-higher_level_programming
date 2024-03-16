0ec96b73b4074537058504133d2ce00b51584a52
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 22:23:18 -0400

    adding task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- 
-SELECT cities.id, cities.state_id, states.name;
-FROM hbtn_0d_usa
-GROUP BY cities.id ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
