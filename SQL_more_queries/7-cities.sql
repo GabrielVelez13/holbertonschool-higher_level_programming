1618622ef2adcc01a8086a3c9518c573ffcc2a3c
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-15 22:11:24 -0400

    adding task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,10 @@
 -- Create a table called cities with the following columns:
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-\c hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-    id SERIAL PRIMARY KEY,
-    state_id INT REFERENCES states(id) NOT NULL,
-    name VARCHAR(256) NOT NULL
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+	PRIMARY KEY(`id`),
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`state_id` INT NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
+	FOREIGN KEY(`state_id`)
+	REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
