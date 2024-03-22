8c55fde5844485d6e720501296760ec178686a65
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-22 00:17:15 -0400

    Fixing models

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
+""" Creating the model for the state class"""
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class State(Base):
+    """ Defining the attributes. """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
