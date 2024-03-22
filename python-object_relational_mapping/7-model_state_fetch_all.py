f7c19c77a00539ffc94aea11fd6cf387e6a3c546
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-22 00:57:38 -0400

    adding task 7, pycodestyle

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,9 +2,10 @@
 """ List all states from a data base. """
 
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
 
 if __name__ == "__main__":
 
