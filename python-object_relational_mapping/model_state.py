a3bc2328741428893c6cf5b12c4fa9e2fce4c817
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-22 00:10:47 -0400

    adding models_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 class State(Base):
-    __tablename__ = "states"
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
