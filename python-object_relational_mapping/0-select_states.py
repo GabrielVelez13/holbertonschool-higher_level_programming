615cd98107d879a5c781662de624094e90ba66cf
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-22 09:02:10 -0400

    checking

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the databse hbtn_0e_0_usa
+"""
+from sys import argv
+import sqlalchemy
+import MySQLdb
+
+if __name__ == '__main__':
+    u_name = argv[1]
+    psw = argv[2]
+    base = argv[3]
+
+    # Connecting to MySQL database
+    db = MySQLdb.connect(host="localhost", user=u_name,
+                         passwd=psw, db=base, port=3306)
+
+    # Creating cursor object
+    cur = db.cursor()
+
+    # Executing MySql Query
+    cur.execute("SELECT * FROM states ORDER BY id")
+
+    # Obtaining Query Result & prints the result in rows
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Clean Up
+    cur.close()
+    db.close()
