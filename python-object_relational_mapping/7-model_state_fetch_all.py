810df2095fe442d7a2c4ff393fd4009dfe83c6a4
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-22 00:44:58 -0400

    adding task 7, if main

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,25 +6,27 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+if __name__ == "__main__":
+        
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-""" Creating the engine. """
-engine = create_engine(
-    f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
-)
-Base.metadata.create_all(engine)
+    """ Creating the engine. """
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    )
+    Base.metadata.create_all(engine)
 
-""" Making the session. """
-Session = sessionmaker(bind=engine)
-session = Session()
+    """ Making the session. """
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-states = session.query(State).all()
+    states = session.query(State).all()
 
-""" Finding the data. """
-for state in states:
-    print(f"{state.id}: {state.name}")
+    """ Finding the data. """
+    for state in states:
+        print(f"{state.id}: {state.name}")
 
-""" Closing the session. """
-session.close()
+    """ Closing the session. """
+    session.close()
