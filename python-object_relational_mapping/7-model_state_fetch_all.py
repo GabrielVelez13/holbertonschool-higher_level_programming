b27d2780b0b24ecba054e44b9283b95d1d4e2213
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-22 00:58:40 -0400

    adding task 7, pycodestyle

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-""" List all states from a data base. """
+""" List all states from the database. """
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    """ Creating the engine. """
     engine = create_engine(
         f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
     )
 
-    """ Making the session. """
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    """ Finding the data. """
-    for states in session.query(State).order_by(State.id):
-        print(f"{states.id}: {states.name}")
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
 
-    """ Closing the session. """
     session.close()
