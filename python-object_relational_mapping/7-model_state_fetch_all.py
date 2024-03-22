088dee03c7e3d8b11a7790f3cdf6bd4fef5a6e27
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-22 00:45:53 -0400

    adding task 7, pycodestyle

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
 if __name__ == "__main__":
-        
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
