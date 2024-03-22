b6b3ea45623e0a60e9d7f8a038566fcfe144d8f5
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-22 01:11:14 -0400

    adding task 7, trying to fix it

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""Lists all states object from the database"""
-
+""" List all states from the database. """
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    """Lists all state object from the database"""
-
-    user = sys.argv[1]
+    """ Doesn't run on import. """
+    username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     engine = create_engine(
-        f"mysql+mysqldb://{user}:{password}@localhost:3306/{database}"
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
     )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for states in session.query(State).all():
+    for states in session.query(State).order_by(State.id).all():
         print(f"{states.id}: {states.name}")
 
     session.close()
