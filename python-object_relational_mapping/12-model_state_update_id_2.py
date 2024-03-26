e69976a18d3c68db4c2c24a4d502e4d26fea8601
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-26 18:48:58 -0400

    task 12 adding

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Adds a new state. """
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """ Receiving information. """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    """ Create engine. """
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    )
+
+    Base.metadata.create_all(engine)
+
+    """ Start session. """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """ Adding new state"""
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.add(state)
+    session.commit()
+
+    """ Close session. """
+    session.close()
