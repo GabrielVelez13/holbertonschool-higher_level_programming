9ef74286b610fc578e0f510790c195b0335261dc
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-26 18:25:16 -0400

    adding task 10, fixing

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -7,13 +7,13 @@ from sqlalchemy.orm import Session, sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    user = sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     """ Create engine. """
     engine = create_engine(
-        f"mysql+mysqldb://{user}:{password}@localhost:3306/{database}"
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
     )
 
     Base.metadata.create_all(engine)
@@ -22,9 +22,9 @@ if __name__ == "__main__":
     session = Session()
 
     """ Look for state. """
-    for state in session.query(State).all():
-        if sys.argv[4] == state.name:
-            print(state.id)
+    for states in session.query(State).order_by(State.id).all():
+        if sys.argv[4] == states.name:
+            print(states.id)
 
     """ Close session. """
     session.close()
