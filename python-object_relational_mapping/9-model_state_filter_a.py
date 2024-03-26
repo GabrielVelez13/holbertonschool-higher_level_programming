d9c9d2cf3ddcf22eb1627b3f984bb805c6c19bc9
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-26 12:02:46 -0400

    checking task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Listing all states in db that have an 'a' in its name. """
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """ Receiving information. """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbName = sys.argv[3]
+
+    """ Creating engine. """
+    engine = create_engine(
+        "mysql+mysqldb://{username}:{password}@localhost:3306/{dbName}"
+    )
+
+    Base.metadata.create_all(engine)
+
+    """ Creating session. """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """ Printing all states with a in its name. """
+
+    for states in session.query(State).order_by(State.id).all():
+        if 'a' in states.name:
+            print(f"{states.id}: {states.name}")
+
+    """ Closing session. """
+    session.close()
+
+
