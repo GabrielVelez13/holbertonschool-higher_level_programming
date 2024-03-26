6163b1aae417bfb9ba651c5bcbaac1f0e5b0136e
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-26 11:43:07 -0400

    Adding task 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """ Doesn't run on import. """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    firstState = session.query(State).order_by(State.id).first()
+    print(f"{firstState.id}: {firstState.name}")
+
+    session.close()
