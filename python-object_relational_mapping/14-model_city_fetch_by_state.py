e6d85d34c3eaf4423baf6518c765e3b8bbb78791
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-03-26 22:14:37 -0400

    adding task 14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,29 @@
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """ Receiving information. """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    """ Create engine. """
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    )
+
+    Base.metadata.create_all(engine)
+
+    """ Start session. """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).all()
+    cities = state.cities
+
+    for city in cities:
+        print(f"{state.name}: {city.id} {city.name}")
+
+    session.close()
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Creating a new class that creates a relationship between two classes. """
+
+from model_state import Base, State
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """ Defining the class City. """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("State.id"), nullable=False)
+
+    State = relationship(State)
