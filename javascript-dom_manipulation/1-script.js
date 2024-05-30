263e72b8ec24d182de1ffee0f1eb780404770633
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-05-30 11:14:34 -0400

    adding last files

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -3,4 +3,4 @@ let red_header = document.querySelector('#red_header');
 red_header.addEventListener('click', function() {
   const header = document.querySelector('header');
   header.style.color = "#FF0000";
-})
+});
